beec2aa3a0f7a2754d1f2ec1fabb85fea9ef508e
Bashir <b_egeh@hotmail.com>
2020-11-24 20:31:04 -0500

    change title of app; add toolbar div into tools sections

--- src/App.js
+++ src/App.js
@@ -9,26 +9,14 @@ function App() {
       </section>
       <section className="main">
         <div className= "tools">
+          <div className="toolbar">
 
+          </div>
         </div>
         <div className="canvas">
 
         </div>
       </section>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
