59cbc44c0f7b16a229bcf2782119abf0c35e0814
Bashir <b_egeh@hotmail.com>
2020-11-25 23:11:34 -0500

    open color picker when color tool is clicked and set color tool value to selected color

--- src/App.js
+++ src/App.js
@@ -1,76 +1,94 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import floodfill from './images/floodfill.png';
-import pencil from './images/pencil.png';
-import { SketchPicker } from 'react-color';
-import { findByLabelText } from '@testing-library/react';
+import "./App.css";
+import { useState, useEffect } from "react";
+import floodfill from "./images/floodfill.png";
+import pencil from "./images/pencil.png";
+import { SketchPicker } from "react-color";
+import { findByLabelText } from "@testing-library/react";
 
 function App() {
-  const [ color, setColor ] = useState('#fff');
+  const [color, setColor] = useState("#fff");
   const [tool, setTool] = useState(null);
   const [dimensions, setDimensions] = useState({ height: 16, width: 32 });
   const { height, width } = dimensions;
+  const [showColorPicker, setShowColorPicker] = useState(false);
 
   const handleChangeColor = (color) => {
     setColor(color.hex);
   };
 
   const createRow = () => {
-    const row = []
-    for(let i = 1; i <= width; i++) {
+    const row = [];
+    for (let i = 1; i <= width; i++) {
       row.push(
-        <div 
+        <div
           key={i}
           className="grey-on-hover squares"
-          style={{width: `calc(80vw/${width})`, height: `calc(80vh/${height})`}} 
+          style={{
+            width: `calc(80vw/${width})`,
+            height: `calc(80vh/${height})`,
+          }}
         />
-      )
+      );
     }
     return row;
-  }
+  };
 
   const createCanvas = () => {
     const canvas = [];
-    for(let i = 1; i <= height; i++) {
+    for (let i = 1; i <= height; i++) {
       canvas.push(
-        <div style={{display: 'flex', msFlexDirection: "row"}}>
-          {
-            createRow()
-          }
+        <div style={{ display: "flex", msFlexDirection: "row" }}>
+          {createRow()}
         </div>
-      )
+      );
     }
     return canvas;
-  }
-
-
+  };
 
   return (
     <div className="App">
-      <SketchPicker
-        color={color}
-        onChange={ (color) => handleChangeColor(color)}
-      />
       <section className="heading">
         <h1>PAINT</h1>
       </section>
       <section className="main">
-        <div className= "tools">
+        <div className="tools">
           <div className="toolbar">
-            <div 
-              className={`tool-btn-container ${tool === 'flood-fill' && "tool-btn-clicked"}`}
-              onClick={() => setTool('flood-fill')}
+            <div
+              className={`tool-btn-container ${
+                tool === "flood-fill" && "tool-btn-clicked"
+              }`}
+              onClick={() => setTool("flood-fill")}
             >
               <img className="tool-btn-img" src={floodfill} />
             </div>
-            <div 
-              className={`tool-btn-container ${tool === 'pencil' && "tool-btn-clicked"}`}
-              onClick={() => setTool('pencil')}
+            <div
+              className={`tool-btn-container ${
+                tool === "pencil" && "tool-btn-clicked"
+              }`}
+              onClick={() => setTool("pencil")}
             >
               <img className="tool-btn-img" src={pencil} />
             </div>
-            <div className="tool-btn-container">
-              <div style={{backgroundColor: color, width: '100%', height: '100%'}} />
+            <div
+              className="tool-btn-container"
+              onClick={() => setShowColorPicker(!showColorPicker)}
+            >
+              <div
+                style={{
+                  backgroundColor: color,
+                  width: "100%",
+                  height: "100%",
+                }}
+              />
+              {showColorPicker && (
+                <div className="popover">
+                  <div className="cover" />
+                  <SketchPicker
+                    color={color}
+                    onChange={(color) => handleChangeColor(color)}
+                  />
+                </div>
+              )}
             </div>
           </div>
         </div>
@@ -78,7 +96,6 @@ function App() {
           {/* <div className="squares"
           style={{width: `calc(80vw/${width})`, height: `calc(80vh/${height})`}}> */}
           {createCanvas()}
-            
         </div>
       </section>
     </div>
