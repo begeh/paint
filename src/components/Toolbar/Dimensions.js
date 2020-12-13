234ba672585cd8c0cb150f2417f39996277b94e7
Bashir <b_egeh@hotmail.com>
2020-12-12 21:05:50 -0500

    update height and width dimensions to be set as number values; make dimensions rounded down integers instead of float values

--- src/components/Toolbar/Dimensions.js
+++ src/components/Toolbar/Dimensions.js
@@ -2,32 +2,32 @@ import DimensionItem from "./DimensionItem";
 
 export default function Dimensions(props) {
   const { dimensions, setDimensions, height, width } = props;
-  const windowHeight = 0.8 * window.innerHeight;
-  const windowWidth = 0.8 * window.innerWidth;
+  const windowHeight = Math.floor(0.8 * window.innerHeight);
+  const windowWidth = Math.floor(0.8 * window.innerWidth);
 
   const handleChangeDimensions = ({ type, value }) => {
     if (type === "height") {
       return Number(value) > windowHeight
         ? setDimensions({ ...dimensions, height: windowHeight })
-        : setDimensions({ ...dimensions, height: value });
+        : setDimensions({ ...dimensions, height: Math.floor(Number(value)) });
     } else if (type === "width") {
       return Number(value) > windowWidth
         ? setDimensions({ ...dimensions, width: windowWidth })
-        : setDimensions({ ...dimensions, width: value });
+        : setDimensions({ ...dimensions, width: Math.floor(Number(value)) });
     }
   };
 
   return (
     <div className="dimensions-container">
       <DimensionItem
-        title={`Canvas Height (max. ${Math.floor(windowHeight)}px)`}
+        title={`Canvas Height (max. ${windowHeight}px)`}
         handleChangeDimensions={handleChangeDimensions}
         type="height"
         placeholder="Height (in px)"
         value={height}
       />
       <DimensionItem
-        title={`Canvas Width (max. ${Math.floor(windowWidth)}px)`}
+        title={`Canvas Width (max. ${windowWidth}px)`}
         handleChangeDimensions={handleChangeDimensions}
         type="width"
         placeholder="Width (in px)"
