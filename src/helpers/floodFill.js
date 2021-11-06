a4e6eeae02a07ee2ae9e3958b77f47ddc61de1e3
Bashir <b_egeh@hotmail.com>
2021-11-06 13:41:13 -0400

    add comments to floodFill function

--- src/helpers/floodFill.js
+++ src/helpers/floodFill.js
@@ -1,4 +1,6 @@
 const floodFill = (canvas, row, column, newColor) => {
+  /* If current square has same color as flood fill color, then
+   exit out of recursive loop */ 
   if (canvas[row][column] === newColor) {
     return canvas;
   }
@@ -7,6 +9,8 @@ const floodFill = (canvas, row, column, newColor) => {
 };
 
 const fill = (canvas, row, column, color, newColor) => {
+  /* Exit out of recursive loop if 1) index of row/column does not exist (i.e. greater than row/column length, or less than zero), or 2) current square is not same color as adjacent square.
+   */
   if (
     row < 0 ||
     column < 0 ||
@@ -17,9 +21,13 @@ const fill = (canvas, row, column, color, newColor) => {
     return;
   }
   canvas[row][column] = newColor;
+  // Check square below
   fill(canvas, row + 1, column, color, newColor);
+  // Check square above
   fill(canvas, row - 1, column, color, newColor);
+  // Check square to the left
   fill(canvas, row, column - 1, color, newColor);
+  // Check square to the right
   fill(canvas, row, column + 1, color, newColor);
   return canvas;
 };
