c759088b3846c6a7e63bf70f1c058f251410c3d4
Bashir <b_egeh@hotmail.com>
2021-11-06 17:22:09 -0400

    remove innerHeight/innerWidth use in height/width calcs in useScreenDimensions.jsx

--- src/helpers/hooks/useScreenDimensions.jsx
+++ src/helpers/hooks/useScreenDimensions.jsx
@@ -1,12 +1,10 @@
 import { useState, useEffect } from "react";
 
 const getWidth = () =>
-  window.innerWidth ||
   document.documentElement.clientWidth ||
   document.body.clientWidth;
 
 const getHeight = () =>
-  window.innerHeight ||
   document.documentElement.clientHeight ||
   document.body.clientHeight;
 
