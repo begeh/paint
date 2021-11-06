074ee470f9d24c18620a3813a17bd8e813725c7f
Bashir <b_egeh@hotmail.com>
2021-11-06 17:26:59 -0400

    revert changes to useScreenDimensions

--- src/helpers/hooks/useScreenDimensions.jsx
+++ src/helpers/hooks/useScreenDimensions.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 
 const getWidth = () =>
+  window.innerWidth ||
   document.documentElement.clientWidth ||
   document.body.clientWidth;
 
 const getHeight = () =>
+  window.innerHeight ||
   document.documentElement.clientHeight ||
   document.body.clientHeight;
 
